20c4e797643607b41069a96c60ca91b2e957e4ef
Rawan <rawnq.sb@gmail.com>
2023-01-29 20:19:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+
+//   // To pass the tests, don't forget to return your fetch!
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -10,6 +8,13 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+}
+
+function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((data) => renderBooks(data));
+  return fetch("https://anapioficeandfire.com/api/books")
 }
 
 document.addEventListener('DOMContentLoaded', function() {
